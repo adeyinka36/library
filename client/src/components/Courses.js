f8cab03b9713018a8b796c570b21b490ad5ccb19
Adeyinka Giwa <adeyinka36@gmail.com>
2020-05-27 02:16:08 +0100

    corrected tostrign error line 35 courses react client

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -32,7 +32,7 @@ class Courses extends Component{
       if(this.state.courses.length>0){
         const content = this.state.courses.map(course =>{
           return(
-              <div key={course.id.toString()} className="grid-33">
+              <div key={course._id.toString()} className="grid-33">
               <Link className="course--module course--link" to={`/courses/${course.id}`}>
                   <h4 className="course--label">Course</h4>
                   <h3 className="course--title">{course.title}</h3>
